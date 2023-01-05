7c5a99a184713156923a3ad088af98ea6e2387c5
mmf09 <mmf09@YPC>
2023-01-04 23:17:10 -0800

    Add project files.

---
+++ Operatorsoverloading/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public class Employee //creating employee class with id properties
+    {
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static bool operator ==(Employee person1, Employee person2) //using == overloading operators for comparison
+        {
+            if (person1 == person2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee person1, Employee person2)
+        {
+            if (person1 != person2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
